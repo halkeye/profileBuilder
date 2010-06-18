0249e9154d4aaa329fc2259954f127f48e4dd589
Gavin <github@gavinmogan.com>
2010-06-18 00:57:57 -0700

    simple little cgi script to test openid handling

--- test.pl
+++ test.pl
@@ -20,39 +20,50 @@
 
 use strict;
 use warnings;
+use CGI::Carp qw(fatalsToBrowser);
 
 use CGI;
 my $cgi = CGI->new;
 
+use Data::Dumper;
   use Net::OpenID::Consumer;
   use LWPx::ParanoidAgent;
-  use Cache::Null;
+  use Cache::File;
 
   my $csr = Net::OpenID::Consumer->new(
     ua    => LWPx::ParanoidAgent->new,
-    cache => Cache::Null->new,
+    cache => Cache::File->new( cache_root => '/tmp/openid') ,
     args  => $cgi,
     consumer_secret => "abc123",
-    required_root => "http://moocow.halkeye.net/",
+    #required_root => "http://localhost/",
+    debug => 1,
   );
 
   # a user entered, say, "bradfitz.com" as their identity.  The first
   # step is to fetch that page, parse it, and get a
   # Net::OpenID::ClaimedIdentity object:
 
-  my $claimed_identity = $csr->claimed_identity("bradfitz.com");
-
   # now your app has to send them at their identity server's endpoint
   # to get redirected to either a positive assertion that they own
   # that identity, or where they need to go to login/setup trust/etc.
 
-  my $check_url = $claimed_identity->check_url(
-    return_to  => "http://example.com/openid-check.app?yourarg=val",
-    trust_root => "http://example.com/",
-  );
-
   # so you send the user off there, and then they come back to
   # openid-check.app, then you see what the identity server said.
+  if (($cgi->param('yourarg')||'') ne 'val')
+  {
+      my $claimed_identity = $csr->claimed_identity("https://www.google.com/accounts/o8/id");
+      $claimed_identity or die $csr->err;
+
+
+      my $check_url = $claimed_identity->check_url(
+        delayed_return => 1,
+        return_to  => "http://localhost/cgi-bin/test.cgi?yourarg=val",
+        trust_root => "http://localhost/",
+      );
+      $csr->_debug("sending off to $check_url");
+      print "Location: $check_url\n\n";
+      exit();
+  }
 
   # Either use callback-based API (recommended)...
   $csr->handle_server_response(
@@ -61,7 +72,10 @@ my $cgi = CGI->new;
       },
       setup_required => sub {
           my $setup_url = shift;
+          die("setup is required @ $setup_url");
           # Redirect the user to $setup_url
+          print "Location: $setup_url\n\n";
+          exit();
       },
       cancelled => sub {
           # Do something appropriate when the user hits "cancel" at the OP
@@ -69,21 +83,13 @@ my $cgi = CGI->new;
       verified => sub {
           my $vident = shift;
           # Do something with the VerifiedIdentity object $vident
+          print $cgi->header;
+          print "Hey there sucka";
       },
       error => sub {
-          my $err = shift;
-          die($err);
+          my ($err,$txt) = shift;
+          $csr->_debug("MEssage version: " . $csr->_message_version);
+          die("Error: $txt($err)");
       },
   );
 
-  # ... or handle the various cases yourself
-  if (my $setup_url = $csr->user_setup_url) {
-       # redirect/link/popup user to $setup_url
-  } elsif ($csr->user_cancel) {
-       # restore web app state to prior to check_url
-  } elsif (my $vident = $csr->verified_identity) {
-       my $verified_url = $vident->url;
-       print "You are $verified_url !";
-  } else {
-       die "Error validating identity: " . $csr->err;
-  }
